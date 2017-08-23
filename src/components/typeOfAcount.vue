87822d96942c47ac895a98daccd0de186f308b41
钱毓婷 <taryn2016@outlook.com>
2017-08-23 23:28:36 +0800

    fix: 添加flexible.js文件

---
+++ src/components/typeOfAcount.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  props: {
+    name: {
+      type: String,
+      default: 'qyt'
+    }
+  },
+  computed: {
+    methodName() {
+
+    }
+  },
+  methods: {
+  },
+  watch: {
+    //
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
